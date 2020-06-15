3c0fda5e8abd68d6de87f2fd3b09dac2f821bbb4
Pranav Krishna <krishnatechpranav@gmail.com>
2020-06-14 21:40:13 -0700

    12

--- js/district.js
+++ js/district.js
@@ -21,6 +21,7 @@ function getDistrictStatus(){
     var _ref = firebase.database().ref().child("UserData").child(email).child('Districts');
 
     _ref.once('value').then(function (snapshot) {
+        console.log(snapshot.val());
         if(snapshot.val() == null){
             document.getElementById('createDistrictOptions').style.display = "none";
         } else {
