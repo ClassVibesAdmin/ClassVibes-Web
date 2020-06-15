58a561056a7c5faf650a31e21b1045d1f522ad32
Pranav Krishna <krishnatechpranav@gmail.com>
2020-06-14 21:45:38 -0700

    12

--- js/district.js
+++ js/district.js
@@ -32,6 +32,23 @@ function getDistrictStatus(){
     });
 }
 
+function getDistrictStatusCreatePage(){
+    var email = localStorage.getItem('email');
+
+    var _ref = firebase.database().ref().child("UserData").child(email).child('Districts');
+
+    _ref.once('value').then(function (snapshot) {
+        console.log(snapshot.val());
+        if(snapshot.val() == null){
+            document.getElementById('createDistrict-page').style.display = "initial";
+            document.getElementById('quotaReached').style.display = "none";
+        } else {
+            document.getElementById('quotaReached').style.display = "initial";
+            document.getElementById('createDistrict-page').style.display = "none";
+        }
+    });
+}
+
 function createDistrict(){
 
     var userEmail = localStorage.getItem('email');
