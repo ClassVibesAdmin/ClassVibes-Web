5a96e026f52c06f6874bcd780d6dfb7aa51b4c0d
HollandPleskac <hollandpleskac@gmail.com>
2020-06-17 14:13:19 -0700

    district to firestore login w/ email password

--- js/auth.js
+++ js/auth.js
@@ -1013,41 +1013,79 @@ function loginWithEmailDistrict() {
 
         if (authValid == true) {
 
-            var _ref = firebase.database().ref().child("UserData").child(formattedEmail).child('Account Type');
+            // var _ref = firebase.database().ref().child("UserData").child(formattedEmail).child('Account Type');
 
-            _ref.once('value').then(function (snapshot) {
-                var exists = snapshot.val();
-                console.log(exists);
+            // _ref.once('value').then(function (snapshot) {
+            //     var exists = snapshot.val();
+            //     console.log(exists);
 
-                if (exists != null) {
-                    if (exists == "District") {
+            //     if (exists != null) {
+            //         if (exists == "District") {
 
-                        console.log('Login Success');
+            //             console.log('Login Success');
 
-                        localStorage.setItem("email", formattedEmail);
+            //             localStorage.setItem("email", formattedEmail);
 
 
-                        window.location = "districtDashboard.html";
-                    } else {
+            //             window.location = "districtDashboard.html";
+            //         } else {
 
-                        errorHTML = `<div class="alert alert-danger" role="alert" 
-                        style="margin-top: 20px; width: 94%; margin-left: 6%;">
-                        <strong>Oops! </strong> This account was signed up as a ${exists} account. You do not have sufficient permissions.
-                    </div>`;
+            //             errorHTML = `<div class="alert alert-danger" role="alert" 
+            //             style="margin-top: 20px; width: 94%; margin-left: 6%;">
+            //             <strong>Oops! </strong> This account was signed up as a ${exists} account. You do not have sufficient permissions.
+            //         </div>`;
 
-                        document.getElementById('signupError').innerHTML = errorHTML;
+            //             document.getElementById('signupError').innerHTML = errorHTML;
 
-                    }
+            //         }
+            //     } else {
+
+            //         errorHTML = `<div class="alert alert-danger" role="alert"
+            //         style="margin-top: 20px; width: 94%; margin-left: 6%;">
+            //         <strong>Error! </strong> An unexpected error has acurred, please contact customer support.
+            //     </div>`;
+
+            //         document.getElementById('signupError').innerHTML = errorHTML;
+            //     }
+            // });
+
+            //NEW CODE
+            //NEW CODE
+            var accountType = firebase.firestore().collection('user data').document(formattedEmail).get().get().then(function (doc) {
+                if (doc.exists) {
+                    console.log("Document data:", doc.data()["Account Type"]);
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                }
+            }).catch(function (error) {
+                console.log("Error getting document:", error);
+            });
+
+            if (accountType != null) {
+                if (accountType == "District") {
+                    console.log('Login Success');
+                    localStorage.setItem("email", formattedEmail);
+                    window.location = "districtDashboard.html";
                 } else {
 
-                    errorHTML = `<div class="alert alert-danger" role="alert"
-                    style="margin-top: 20px; width: 94%; margin-left: 6%;">
-                    <strong>Error! </strong> An unexpected error has acurred, please contact customer support.
-                </div>`;
+                    errorHTML = `<div class="alert alert-danger" role="alert" 
+                style="margin-top: 20px; width: 94%; margin-left: 6%;">
+                <strong>Oops! </strong> This account was signed up as a ${exists} account. You do not have sufficient permissions.
+            </div>`;
 
                     document.getElementById('signupError').innerHTML = errorHTML;
+
                 }
-            });
+            } else {
+
+                errorHTML = `<div class="alert alert-danger" role="alert"
+            style="margin-top: 20px; width: 94%; margin-left: 6%;">
+            <strong>Error! </strong> An unexpected error has acurred, please contact customer support.
+            </div>`;
+
+                document.getElementById('signupError').innerHTML = errorHTML;
+            }
 
         }
     });
