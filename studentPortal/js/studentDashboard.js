58b82bcaadfee627f8c8a39bca36995929ee6e4f
Pranav Krishna <krishnatechpranav@gmail.com>
2020-06-23 10:50:01 -0700

    12

--- studentPortal/js/studentDashboard.js
+++ studentPortal/js/studentDashboard.js
@@ -127,6 +127,79 @@ function getStudentClasses(studentUsername) {
 
   classesList = [];
 
+  firebase.firestore().collection('UserData').doc(studentUsername).Collection("Classes").get().then(function (doc) {
+    var classes = doc.data();
+
+    console.log(classes);
+
+    if(classes != null){
+      classes.forEach((child) => {
+        var classCode = child["Code"];
+        var className = child["class-name"];
+
+        classesList.push(className);
+        classCodes[className] = classCode;
+      }).then(() => {
+        onsole.log(classesList.length);
+
+    console.log(classesList);
+
+    inital = `
+        <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="selectedClassForDropdown">
+                  ${classesList[0]}
+                </button>
+        `;
+
+    selectedClass = classesList[0];
+
+    $(inital).appendTo("#selectedClassForDropdown");
+
+
+    classesList.forEach(function (item, index) {
+      console.log(item, index);
+      output = `
+          <div class="col-xl-3 col-md-6 mb-4">
+                  <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                      <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                          <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">CLASS</div>
+                          <div class="h5 mb-0 font-weight-bold text-gray-800">${item}</div>
+                        </div>
+                        <div class="col-auto">
+                          <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+          `;
+
+      output2 = `
+          <a class="collapse-item">${item}</a>
+          `;
+
+      output3 = `
+          <div class="dropdown-item" onclick="setMainClassForMood('${item}', '${classesList[0]}')" id = "${item}">${item}</div>
+          `;
+
+      dropDownMenuItems += output3;
+
+      $(output3).appendTo("#dropDownMoodPicker");
+
+      $(output2).appendTo("#classesListSideBar");
+
+      $(output).appendTo("#classesRowDisplay");
+    });
+      });
+    } else {
+
+    }
+
+  });
+
+  /*
+
   var _ref = firebase.database().ref().child("UserData").child(studentUsername).child("Classes");
 
   _ref.once('value').then(function (snapshot) {
@@ -196,6 +269,7 @@ function getStudentClasses(studentUsername) {
       $(output).appendTo("#classesRowDisplay");
     });
   });
+  */
 
 }
 
