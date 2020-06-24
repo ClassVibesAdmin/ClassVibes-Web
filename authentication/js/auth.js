b8c77e468a9f3f075005360c6b23b52a7c0cab45
Pranav Krishna <krishnatechpranav@gmail.com>
2020-06-24 10:17:07 -0700

    12

--- authentication/js/auth.js
+++ authentication/js/auth.js
@@ -1095,7 +1095,7 @@ function loginWithEmailStudent() {
 
                             console.log(email);
 
-                            console.log(localStorage.getItem("email"));
+                            console.log(document.cookie);
 
                             setTimeout(() => { window.location = "../studentPortal/studentDashboard.html"; }, 3000)
 
