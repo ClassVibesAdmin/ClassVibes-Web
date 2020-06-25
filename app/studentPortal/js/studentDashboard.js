b2a634008db20a244f057586a0ce7ce29fc07d5d
Pranav Krishna <krishnatechpranav@gmail.com>
2020-06-24 20:27:11 -0700

    345

--- app/studentPortal/js/studentDashboard.js
+++ app/studentPortal/js/studentDashboard.js
@@ -785,7 +785,7 @@ function getMeetings(pageType) {
         console.log(date);
   
         output = `
-          <div class="col-xl-12 col-md-6 mb-4">
+          <div class="col-xl-6 col-md-6 mb-4">
           <div class="card border-left-primary shadow h-100 py-2">
             <div class="card-body">
               <div class="row no-gutters align-items-center">
