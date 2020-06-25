eac9bcd19724b94bc8bfca7ab22b1038202d4990
Pranav Krishna <krishnatechpranav@gmail.com>
2020-06-24 20:23:08 -0700

    234

--- app/studentPortal/js/studentDashboard.js
+++ app/studentPortal/js/studentDashboard.js
@@ -731,6 +731,7 @@ function getMeetings(pageType) {
                 <div class="col mr-2">
                   <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">${classForMeeting}</div>
                   <div class="h5 mb-0 font-weight-bold text-gray-800">${title}</div>
+                  <h4 style = 'color: gray'>${date}</h4>
                 </div>
                 <div class="col-auto">
   
