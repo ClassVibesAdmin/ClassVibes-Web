cfd58cf240185df24a62f9fc307515291ea549de
Pranav Krishna <krishnatechpranav@gmail.com>
2020-07-04 12:57:45 -0700

    345

--- app/studentPortal/js/studentDashboard.js
+++ app/studentPortal/js/studentDashboard.js
@@ -388,9 +388,7 @@ function updateReaction(reaction) {
 
   console.log(classCodes);
 
-  firebase.firestore().collection("Classes").doc(classSelected).collection("Student Reactions").doc().set({
-    studentEmail: studentEmail,
-    reaction: reaction,
+  firebase.firestore().collection("UserData").doc(`${studentEmail}/Last Status Update${classSelected}`).collection("Student Reactions").doc().set({
     date: currentDate.toString()
   });
 
