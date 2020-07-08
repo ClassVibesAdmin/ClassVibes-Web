4981c7aae084963c9c5ee52c4b1a19acb725599c
Pranav Krishna <krishnatechpranav@gmail.com>
2020-07-08 13:31:53 -0700

    345

--- app/studentPortal/js/studentDashboard.js
+++ app/studentPortal/js/studentDashboard.js
@@ -714,10 +714,7 @@ function getStudentStatus() {
   var page = document.getElementById('currentStatusSection');
 
   firebase.firestore().collection('UserData').doc(studentEmail).get().then(function (doc) {
-    var value = doc.data()["Reaction"];
-
-    console.log("REACTION:" + value)
-
+    var value = doc.data()["reaction"];
 
     if (value != undefined) {
       if (value == "needs help") {
