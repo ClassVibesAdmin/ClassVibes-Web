57ab5dedef3db9f1d54bf9aefb1f2356982c1029
Pranav Krishna <krishnatechpranav@gmail.com>
2020-07-10 12:06:00 -0700

    345

--- app/app_config.js
+++ app/app_config.js
@@ -8,7 +8,10 @@ GLOBAL CONFIG
 function setVersionFooter(){
     var VERSION_CODE = '0.9.0-beta'
 
-    document.getElementById('footer-version-code').innerHTML = `
+    if(document.getElementById('footer-version-code') != null){
+        document.getElementById('footer-version-code').innerHTML = `
         <span>Copyright &copy; Xeon Developers. v${VERSION_CODE}</span>
     `;
+    }
+
 }
